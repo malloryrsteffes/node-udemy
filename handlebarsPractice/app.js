9a51869c5e04f4e7f3570c8576f030dbbf654e3f
Mal <malrsteffes@gmail.com>
2020-01-12 13:42:09 -0500

    layout implemented with handlebars

--- handlebarsPractice/app.js
+++ handlebarsPractice/app.js
@@ -5,7 +5,13 @@ const bodyParser = require("body-parser");
 const expressHbs = require("express-handlebars");
 const app = express();
 
-app.engine("handlebars", expressHbs());
+app.engine(
+  "handlebars",
+  expressHbs({
+    layoutsDir: "views/layouts",
+    defaultLayout: "main-layout"
+  })
+);
 app.set("view engine", "handlebars");
 app.set("views", "views");
 
@@ -19,7 +25,13 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found!" });
+  res
+    .status(404)
+    .render("404", {
+      pageTitle: "Page Not Found!",
+      productCSS: true,
+      formsCSS: true
+    });
 });
 
 app.listen(3000);
